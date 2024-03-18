f9b9e5d7f9293c039ee0608bed89493201775d02
lmh17ever <dug-danya@yandex.ru>
2024-03-18 11:47:39 +0300

    fixed

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+
+from users.forms import CustomUserCreationForm
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -14,7 +15,7 @@ urlpatterns = [
         'auth/registration/',
         CreateView.as_view(
             template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
+            form_class=CustomUserCreationForm,
             success_url=reverse_lazy('pages:homepage'),
             ),
         name='registration'
